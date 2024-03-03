cce2d80e0ee92ef3b38b396b200c662ec92b2e15
ccutsb <cristiancortes9297@gmail.com>
2024-03-03 14:51:33 -0300

    funcionalidades onclick y script.js

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+
+let precioBase = 400000;
+let cantidadBase = 1;
+
+let precio = document.querySelector(".precio");
+let cantidad = document.querySelector(".cantidad");
+let total = document.querySelector(".total");
+
+precio.innerHTML = precioBase;
+cantidad.innerHTML = cantidadBase;
+total.innerHTML = precioBase;
